3df4fe683af30f9bc08b02f5edf487cfd46993bd
cr10Dev <75941095+cr10Dev@users.noreply.github.com>
2024-01-19 17:33:04 +0000

    hfduidgd

--- HeimdallSource/HeimdallSource.cpp
+++ HeimdallSource/HeimdallSource.cpp
@@ -1,23 +1,16 @@
 // HeimdallSource.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-#pragma comment(lib, "d3d11.lib")
 
-#include <iostream>
 #include "WindowHandler.h"
 
+// Main code
+//int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char*, int nShowCmd)
+//{
+//    WindowHandler window;
+//    window.Display();
+//    
+//}
 
-int main()
-{
-    
+int main() {
+	WindowHandler window;
+	window.Display();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- HeimdallSource/WindowHandler.h
+++ HeimdallSource/WindowHandler.h
@@ -1,23 +1,21 @@
 #ifndef WINDOW_HANDLER
 #define WINDOW_HANDLER
 
+#include "imgui.h"
+#include "imgui_impl_win32.h"
+#include "imgui_impl_dx11.h"
 #include <d3d11.h>
 #include <tchar.h>
 
-static ID3D11Device* g_pd3dDevice = nullptr;
-static ID3D11DeviceContext* g_pd3dDeviceContext;
-static UINT g_ResizeWidth = 0, gResizeHeight = 0;
-static ID3D11RenderTargetView* g_mainRenderTargetView = nullptr;
-
-bool CreateDeviceD3D(HWND hWnd);
-void CleanupDeviceD3D();
-void CreateRenderTarget();
-void CleanupRenderTarget();
-LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 class WindowHandler {
 public:
 	int Display();
+	
+	bool CreateDeviceD3D(HWND hWnd);
+	void CleanupDeviceD3D();
+	void CreateRenderTarget();
+	void CleanupRenderTarget();
 };
 
 #endif
